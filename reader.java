b51f1a99520f0faa24b685c8439d50ffab969b61
gracengina <gracemwangi412@gmail.com>
2025-04-23 15:34:51 +0300

    reader.java

--- reader.java
+++ reader.java
@@ -9,10 +9,9 @@ public class reader{
 	public static void file(String[] args) throws IOException {
 		Path path=Path.of("C:" ,"Users", "USER","OneDrive","Desktop","readme");
 		String content=Files.readString(path);
-		int count=0;
-		for(int i:content.toCharArray()) {
-			count++;
-		}
-		System.out.print("There are" +count +"characters in this file");
+		String String[] words = content.split("\\s+");
+		int number=int number=words.length;
+
+		System.out.print("There are" +number +"words in this file");
 	}
 }
